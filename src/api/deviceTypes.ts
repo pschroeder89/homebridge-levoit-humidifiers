da2878d10db102a4ededaf12bb47dfc4433dead1
Paul Schroeder <pschroeder@splunk.com>
2022-01-04 20:50:00 -0700

    sanitize the devicetype

--- src/api/deviceTypes.ts
+++ src/api/deviceTypes.ts
@@ -1,37 +1,42 @@
 export enum DeviceName {
-  Classic300S = 'Classic300S',
-  Classic200S = "Classic200S",
-  Dual200S = 'Dual200S',
+    Classic300S = 'classic300s',
+    Classic200S = "classic200s",
+    Dual200S = 'dual200s',
 }
 
 export interface DeviceType {
-  isValid: (input: string) => boolean;
-  hasAutoMode: boolean;
-  mistLevels: number;
-  mistMinLevel: number;
+    isValid: (input: string) => boolean;
+    hasAutoMode: boolean;
+    mistLevels: number;
+    mistMinLevel: number;
+}
+
+function sanitizeString(input) {
+    // Lowercase the input and remove all whitespace
+    return input.toLowerCase().replace(/\s+/g, '');
 }
 
 const deviceTypes: DeviceType[] = [
-  {
-    isValid: (input: string) =>
-      input.includes(DeviceName.Classic300S),
-    hasAutoMode: true,
-    mistMinLevel: 1,
-    mistLevels: 9
-  },
-  {
-    isValid: (input: string) =>
-        input.includes(DeviceName.Classic200S),
-    hasAutoMode: true,
-    mistMinLevel: 1,
-    mistLevels: 9
-  },
-  {
-    isValid: (input: string) => input.includes(DeviceName.Dual200S),
-    hasAutoMode: true,
-    mistMinLevel: 1,
-    mistLevels: 2
-  },
+    {
+        isValid: (input: string) =>
+            sanitizeString(input).includes(DeviceName.Classic300S),
+        hasAutoMode: true,
+        mistMinLevel: 1,
+        mistLevels: 9
+    },
+    {
+        isValid: (input: string) =>
+            sanitizeString(input).includes(DeviceName.Classic200S),
+        hasAutoMode: true,
+        mistMinLevel: 1,
+        mistLevels: 9
+    },
+    {
+        isValid: (input: string) => sanitizeString(input).includes(DeviceName.Dual200S),
+        hasAutoMode: true,
+        mistMinLevel: 1,
+        mistLevels: 2
+    },
 ];
 
 export default deviceTypes;
