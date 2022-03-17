221dd5a5474c926d9a00b42ced382f055bd84288
Paul Schroeder <pschroeder@splunk.com>
2022-03-16 21:14:12 -0600

    v1.6.1: add support for LV600 UK and EU models

--- src/api/deviceTypes.ts
+++ src/api/deviceTypes.ts
@@ -4,7 +4,9 @@ export enum DeviceName {
     Dual200S = 'Dual200S',
     Dual200S_EU = 'LUH-D301S-WEU',
     Dual200S_UK = 'LUH-D301S-WUK',
-    LV600S = "LUH-A602S-WUS"
+    LV600S = "LUH-A602S-WUS",
+    LV600S_EU = "LUH-A602S-WEU",
+    LV600S_UK = "LUH-A602S-WUK"
 }
 
 export interface DeviceType {
@@ -75,6 +77,30 @@ const deviceTypes: DeviceType[] = [
     {
         isValid: (input: string) =>
             input.includes(DeviceName.LV600S),
+        hasAutoMode: true,
+        coolMistLevels: 9,
+        hasLight: false,
+        hasSleepMode: true,
+        hasWarmMode: true,
+        warmMistLevels: 3,
+        minHumidityLevel: 40,
+        maxHumidityLevel: 80
+    },
+    {
+        isValid: (input: string) =>
+            input.includes(DeviceName.LV600S_EU),
+        hasAutoMode: true,
+        coolMistLevels: 9,
+        hasLight: false,
+        hasSleepMode: true,
+        hasWarmMode: true,
+        warmMistLevels: 3,
+        minHumidityLevel: 40,
+        maxHumidityLevel: 80
+    },
+    {
+        isValid: (input: string) =>
+            input.includes(DeviceName.LV600S_UK),
         hasAutoMode: true,
         coolMistLevels: 9,
         hasLight: false,
