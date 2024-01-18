089a04e10412353670a1b9a956b76c08fee08fd3
Paul Schroeder <paulschroeder@Pauls-Mac-mini.localdomain>
2024-01-17 20:16:36 -0700

    67: add oasismist 600s kus model

--- src/characteristics/SleepState.ts
+++ src/characteristics/SleepState.ts
@@ -29,11 +29,13 @@ const characteristic: {
         await this.device.changeMode(Mode.Sleep);
         break;
       case false:
-        // LEH_S601S_WUS and LUH_O601S_WUS has an auto and humidity mode, we want to revert to humidity for that model since Auto has its own switch
+        // LEH_S601S_WUS, LUH_O601S_KUS, LUH_O601S_WUS has an auto and humidity mode, we want to revert to humidity for that model since Auto has its own switch
         if (
-          [DeviceName.LEH_S601S_WUS, DeviceName.LUH_O601S_WUS].includes(
-            this.device.model as DeviceName,
-          )
+          [
+            DeviceName.LEH_S601S_WUS,
+            DeviceName.LUH_O601S_WUS,
+            DeviceName.LUH_O601S_KUS,
+          ].includes(this.device.model as DeviceName)
         ) {
           await this.device.changeMode(Mode.Humidity);
           break;
